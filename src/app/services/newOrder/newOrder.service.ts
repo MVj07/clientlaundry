5fa67c4bcebb16831520f268888ed5bf410b1cbf
maduraiveeran <maduraiveeran@vprc.in>
2025-09-07 23:19:28 +0530

    changes

--- src/app/services/newOrder/newOrder.service.ts
+++ src/app/services/newOrder/newOrder.service.ts
@@ -5,8 +5,8 @@ import { StorageService } from '../storage.service';
 
 @Injectable({ providedIn: 'root' })
 export class newOrderService {
-    // private apiUrl = 'https://laundry-fju0.onrender.com'; // Change to your actual API URL
-    private apiUrl = 'http://localhost:5000'
+    private apiUrl = 'https://laundry-fju0.onrender.com'; // Change to your actual API URL
+    // private apiUrl = 'http://localhost:5000'
 
     constructor(private http: HttpClient, private storageService: StorageService) { }
 
