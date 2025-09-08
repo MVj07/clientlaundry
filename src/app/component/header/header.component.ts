3fe471b305c13b360f820f3760df7bf81d068b14
surya.m <thiraviyam@firebeetechnoservices.com>
2025-09-08 19:28:10 +0530

    search and packing

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { StorageService } from '../../services/storage.service';
+import { newOrderService } from '../../services/newOrder/newOrder.service';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,22 @@ import { StorageService } from '../../services/storage.service';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  searchText: string = '';
+  dateOrdersmnth :any
   constructor(
     private router: Router,
-    private storageService: StorageService
+    private storageService: StorageService,
+    private orderService: newOrderService
   ){}
+
+  search() {
+    const payload = { search: this.searchText };
+  
+    this.orderService.overallsearch(payload).subscribe((res:any) => {
+      this.dateOrdersmnth = res.data;
+    });
+  }
   logout():void{
     this.storageService.removeItem('authToken')
     this.router.navigate(['/'])
--- src/app/services/newOrder/newOrder.service.ts
+++ src/app/services/newOrder/newOrder.service.ts
@@ -5,8 +5,8 @@ import { StorageService } from '../storage.service';
 
 @Injectable({ providedIn: 'root' })
 export class newOrderService {
-    private apiUrl = 'https://laundry-fju0.onrender.com'; // Change to your actual API URL
-    // private apiUrl = 'http://localhost:5000'
+    // private apiUrl = 'https://laundry-fju0.onrender.com'; // Change to your actual API URL
+    private apiUrl = 'http://localhost:5000'
 
     constructor(private http: HttpClient, private storageService: StorageService) { }
 
@@ -71,5 +71,12 @@ export class newOrderService {
             Authorization: `Bearer ${token}`
         };
     return this.http.post(this.apiUrl + '/items/delete', data,{headers});
+  }
+  overallsearch(data:any): Observable<any>{
+        const token = this.storageService.getItem('authToken'); // or sessionStorage
+     const headers = {
+            Authorization: `Bearer ${token}`
+        };
+    return this.http.post(this.apiUrl + '/items/search', data,{headers});
   }
 }
